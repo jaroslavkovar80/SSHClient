f2aabf0fb943b7416f7c5a7fbc86f31f4072ecf6
Jaroslav Kovar <jaroslav.kovar80@gmail.com>
2023-01-17 22:55:14 +0100

    refactoring

--- GUI/TCMonitorGUI.py
+++ GUI/TCMonitorGUI.py
@@ -2,12 +2,17 @@ from GUI.qt5TCMonitorMainWindow import Ui_MainWindow
 
 class TCMonitorMainWindow(Ui_MainWindow):
     def __init__(self, window):
+
         self.setupUi(window)
+
+        self.tabWidget.setStyleSheet("QTabWidget:title {background-color: red;}")
 
         self.btnTab1.clicked.connect(self.nav_to_tab0)
         self.btnTab2.clicked.connect(self.nav_to_tab1)
         self.btnTab3.clicked.connect(self.nav_to_tab2)
         self.btnTab4.clicked.connect(self.nav_to_tab3)
+
+
 
     def nav_to_tab0(self):
         self.tabWidget.setCurrentIndex(0)
