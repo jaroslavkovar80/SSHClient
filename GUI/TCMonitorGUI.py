f031c02e967d4e4af5a7c63744381040a37f17a1
kovarj <jaroslav.kovar80@gmail.com>
2023-01-20 13:51:37 +0100

    Menu a popup window about

--- GUI/TCMonitorGUI.py
+++ GUI/TCMonitorGUI.py
@@ -1,39 +1,54 @@
 from GUI.qt5TCMonitorMainWindow import Ui_MainWindow
+from GUI.qt5TCMonitorAboutWindow import Ui_Dialog
 from PyQt5.QtWidgets import (QApplication, QTableWidget, QTableWidgetItem)
 from PyQt5.QtCore import QTimer
 import time
 
 from TCMonitorLogic import PUViewer
 
+
+class TCMonitorAboutWindow(Ui_Dialog):
+    def __init__(self,window):
+        self.setupUi(window)
+        super().__init__()
+        #self.resize(300, 300)
+
+
 class TCMonitorMainWindow(Ui_MainWindow):
     def __init__(self, window):
 
         self.setupUi(window)
 
-        #create instace of logic class
+        # create instace of logic class
         self.viewerLogic = PUViewer()
         self._timerMemoryLogging = QTimer()
 
-        #buttons to change index of current tab
+        # create instace of another windows
+        self._aboutWindow = TCMonitorAboutWindow(window)
+
+        # buttons to change index of current tab
         self.btnTab1.clicked.connect(self.nav_to_tab0)
         self.btnTab2.clicked.connect(self.nav_to_tab1)
         self.btnTab3.clicked.connect(self.nav_to_tab2)
         self.btnTab4.clicked.connect(self.nav_to_tab3)
 
-        #butttons of memory table
+        # butttons of memory table
         self.btnAddRecordToMemoryTable.clicked.connect(self.addOneRowToMemoryTable)
         self.btnDelRecordFromMemoryTable.clicked.connect(self.delRowFromMemoryTable)
         self.btnClearMemoryTable.clicked.connect(self.clearMemoryTable)
         self.btnStartMemoryLogging.clicked.connect(self._memoryLoggingManagement)
 
-        #estabilish connection logic
-        self.btnCheckConnection.clicked.connect(self._connectToTarget)
+        # estabilish connection logic
+        # self.btnCheckConnection.clicked.connect(self._connectToTarget)
+        self.btnCheckConnection.clicked.connect(self._showAbout)
 
         self.btnCallCmd.clicked.connect(self._callCommand)
 
-        #update status
+        # update status
         self._updateStatus()
 
+    def _showAbout(self):
+        self._aboutWindow.show()
 
     def _connectToTarget(self):
         if self.viewerLogic.isConnected():
@@ -90,26 +105,26 @@ class TCMonitorMainWindow(Ui_MainWindow):
 
     def addRowToMemoryTable(self, parList):
 
-        #add new empty row
+        # add new empty row
         rowCount = self.tablewMemoryOverview.rowCount()
         self.tablewMemoryOverview.insertRow(rowCount)
 
-        #add current time to first column
+        # add current time to first column
         columnIdx = 0
         currentTime = time.strftime('%X %x')
         rowItem = QTableWidgetItem(currentTime)
         rowItem.setTextAlignment(0x0080 | 0x0004)
         self.tablewMemoryOverview.setItem(rowCount, columnIdx, rowItem)
 
-        #fill rest of column from received list
+        # fill rest of column from received list
         columnIdx += 1
         for item in parList:
             rowItem = QTableWidgetItem(item)
-            rowItem.setTextAlignment(0x0080|0x0004)
-            self.tablewMemoryOverview.setItem(rowCount,columnIdx,rowItem)
+            rowItem.setTextAlignment(0x0080 | 0x0004)
+            self.tablewMemoryOverview.setItem(rowCount, columnIdx, rowItem)
             columnIdx += 1
 
-            #in visu there is only 10 column available, if more items in list -> break it
+            # in visu there is only 10 column available, if more items in list -> break it
             if (columnIdx == 10):
                 break
 
@@ -119,14 +134,14 @@ class TCMonitorMainWindow(Ui_MainWindow):
         rowCount = self.tablewMemoryOverview.currentRow()
 
         # if row is not selected, remove last row
-        if(rowCount==-1):
-            rowCount = self.tablewMemoryOverview.rowCount()-1
+        if (rowCount == -1):
+            rowCount = self.tablewMemoryOverview.rowCount() - 1
 
-        #valid index of the row? -> remove row
+        # valid index of the row? -> remove row
         if rowCount > -1:
             self.tablewMemoryOverview.removeRow(rowCount)
 
-        #print(str(rowCount))
+        # print(str(rowCount))
 
     def clearMemoryTable(self):
 
@@ -134,25 +149,24 @@ class TCMonitorMainWindow(Ui_MainWindow):
 
     def _memoryLoggingManagement(self):
 
-        #loggingTime = int(self.spinBoxLoggingTime.value,default=1)
+        # loggingTime = int(self.spinBoxLoggingTime.value,default=1)
 
         loggingTime = self.spinBoxLoggingTime.value()
 
-        #print(loggingTime)
+        # print(loggingTime)
         if self._timerMemoryLogging.isActive():
             self._timerMemoryLogging.stop()
         elif self.viewerLogic.isConnected():
             self._timerMemoryLogging.timeout.connect(self.cyclicLogic)
-            self._timerMemoryLogging.start(loggingTime*60*1000)
+            self._timerMemoryLogging.start(loggingTime * 60 * 1000)
             self.addOneRowToMemoryTable()
 
         self._updateGUI()
 
-
     def cyclicLogic(self):
-        #print("tick:" + str(time.strftime('%X %x %Z')))
-        #time.sleep(60.0 - ((time.time() - self._startTime) % 60.0))
-        #if (60.0 - ((time.time() - self._startTime) % 60.0)) == 0:
-        #print("tick:" + str(time.strftime('%X %x %Z')))
+        # print("tick:" + str(time.strftime('%X %x %Z')))
+        # time.sleep(60.0 - ((time.time() - self._startTime) % 60.0))
+        # if (60.0 - ((time.time() - self._startTime) % 60.0)) == 0:
+        # print("tick:" + str(time.strftime('%X %x %Z')))
         # time.sleep(30)
         self.addOneRowToMemoryTable()
