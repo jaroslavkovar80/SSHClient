30b6e1b7532e194deb6501c77744522b08727dc9
Jaroslav Kovar <jaroslav.kovar80@gmail.com>
2023-01-23 21:56:44 +0100

    pictures, pokus o style

--- GUI/qt5TCMonitorAboutWindow.py
+++ GUI/qt5TCMonitorAboutWindow.py
@@ -12,18 +12,43 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class Ui_Dialog(object):
-    def setupUi(self,Dialog):
+    def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
+        Dialog.setWindowModality(QtCore.Qt.NonModal)
+        Dialog.setEnabled(True)
+        Dialog.resize(413, 391)
+        Dialog.setStyleSheet("#Dialog{\n"
+"    border-style:none;\n"
+"}\n"
+"#frame{\n"
+"    background-color:#16191d;\n"
+"    border-style:none;\n"
+"}\n"
+"")
         Dialog.setModal(True)
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(60, 70, 47, 13))
-        self.label.setObjectName("label")
+        self.gridLayout = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setSpacing(0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.frame = QtWidgets.QFrame(Dialog)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.btnClose = QtWidgets.QPushButton(self.frame)
+        self.btnClose.setGeometry(QtCore.QRect(200, 160, 75, 23))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/test/pictures/activity.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.btnClose.setIcon(icon)
+        self.btnClose.setIconSize(QtCore.QSize(24, 24))
+        self.btnClose.setObjectName("btnClose")
+        self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
 
         self.retranslateUi(Dialog)
+        self.btnClose.clicked.connect(Dialog.close)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label.setText(_translate("Dialog", "TextLabel"))
+        Dialog.setWindowTitle(_translate("Dialog", "About"))
+        self.btnClose.setText(_translate("Dialog", "PushButton"))
+import GUI.test
