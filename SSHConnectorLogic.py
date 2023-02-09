c08af1accaf590e9fbaa9f37b846b577455d2e31
kovarj <jaroslav.kovar80@gmail.com>
2023-02-09 15:03:36 +0100

    added support for tab Process usage

--- SSHConnectorLogic.py
+++ SSHConnectorLogic.py
@@ -6,6 +6,9 @@ import string
 IP_ADDRESS_REGEX='^(([01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}([01]?\d{1,2}|2[0-4]\d|25[0-5])$'
 CMD_GET_MEMORY = "free -h"
 CMD_GET_DISK_USAGE = "df -h"
+CMD_GET_DMESG = "dmesg"
+CMD_GET_JOURNAL = "journalctl"
+CMD_GET_PROCESS = "ps aux"
 
 class SSHConnector:
     """
@@ -146,8 +149,8 @@ class SSHConnector:
 
         if (self._connected == True):
             receivedList = self._executeCmd(command)
-        else:
-            receivedList = ['Filesystem                Size      Used Available Use% Mounted on\n', '/dev/root               985.4M    358.6M    571.7M  39% /\n', 'devtmpfs                372.0M         0    372.0M   0% /dev\n', 'tmpfs                   500.5M         0    500.5M   0% /dev/shm\n', 'tmpfs                   200.2M      2.2M    198.0M   1% /run\n', 'tmpfs                     4.0M         0      4.0M   0% /sys/fs/cgroup\n', 'tmpfs                   200.2M      2.2M    198.0M   1% /etc/machine-id\n', 'tmpfs                   500.5M         0    500.5M   0% /tmp\n', 'tmpfs                     1.0M         0      1.0M   0% /etc/dropbear\n', 'tmpfs                     5.0M     64.0K      4.9M   1% /home/ppc50-user/.cache\n', 'tmpfs                     5.0M         0      5.0M   0% /home/ppc50-user/.local\n', 'tmpfs                     5.0M         0      5.0M   0% /home/ppc50-user/.dbus\n', 'tmpfs                     5.0M         0      5.0M   0% /home/ppc50-user/.pki\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /var/volatile\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /var/cache\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /srv\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /var/spool\n', '/dev/mmcblk0p1          120.0M     23.0M     88.0M  21% /mnt/sysconfig\n', '/dev/mmcblk0p3          120.0M      1.5M    109.5M   1% /mnt/config\n', '/dev/mmcblk0p4          487.9M    396.0K    451.7M   0% /mnt/user\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /var/lib\n', 'tmpfs                   100.1M     48.0K    100.1M   0% /run/user/1000\n']
+        #else:
+            #receivedList = ['Filesystem                Size      Used Available Use% Mounted on\n', '/dev/root               985.4M    358.6M    571.7M  39% /\n', 'devtmpfs                372.0M         0    372.0M   0% /dev\n', 'tmpfs                   500.5M         0    500.5M   0% /dev/shm\n', 'tmpfs                   200.2M      2.2M    198.0M   1% /run\n', 'tmpfs                     4.0M         0      4.0M   0% /sys/fs/cgroup\n', 'tmpfs                   200.2M      2.2M    198.0M   1% /etc/machine-id\n', 'tmpfs                   500.5M         0    500.5M   0% /tmp\n', 'tmpfs                     1.0M         0      1.0M   0% /etc/dropbear\n', 'tmpfs                     5.0M     64.0K      4.9M   1% /home/ppc50-user/.cache\n', 'tmpfs                     5.0M         0      5.0M   0% /home/ppc50-user/.local\n', 'tmpfs                     5.0M         0      5.0M   0% /home/ppc50-user/.dbus\n', 'tmpfs                     5.0M         0      5.0M   0% /home/ppc50-user/.pki\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /var/volatile\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /var/cache\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /srv\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /var/spool\n', '/dev/mmcblk0p1          120.0M     23.0M     88.0M  21% /mnt/sysconfig\n', '/dev/mmcblk0p3          120.0M      1.5M    109.5M   1% /mnt/config\n', '/dev/mmcblk0p4          487.9M    396.0K    451.7M   0% /mnt/user\n', 'tmpfs                    70.0M      5.4M     64.6M   8% /var/lib\n', 'tmpfs                   100.1M     48.0K    100.1M   0% /run/user/1000\n']
 
         if len(receivedList) > 1:
             header = receivedList[:1]
