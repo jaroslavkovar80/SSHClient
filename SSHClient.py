68097e659850d8d38ea1f6b47e7ec1ed0f8886db
kovarj <jaroslav.kovar80@gmail.com>
2023-05-30 09:06:52 +0200

    V 1.0.2 - small bugs fix, screen size update and windows exe file was added

--- SSHClient.py
+++ SSHClient.py
@@ -6,6 +6,7 @@ from GUI.SSHClientGUI import MainWindow
 if __name__ == "__main__":
     while True:
 
+
         app = QtWidgets.QApplication(sys.argv)
         mainWindow = QtWidgets.QMainWindow()
         ui = MainWindow(mainWindow)
