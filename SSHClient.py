002401b4424caeb45766b4a5ea34f80620fbdc1a
Jaroslav Kovar <jaroslav.kovar@br-automation.com>
2023-06-02 14:42:35 +0200

    comments update

--- SSHClient.py
+++ SSHClient.py
@@ -6,7 +6,7 @@ from GUI.SSHClientGUI import MainWindow
 if __name__ == "__main__":
     while True:
 
-
+        #application can be started only from this file
         app = QtWidgets.QApplication(sys.argv)
         mainWindow = QtWidgets.QMainWindow()
         ui = MainWindow(mainWindow)
